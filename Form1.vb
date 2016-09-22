cd59f5d506e4686baf2f807ddc13b435c1237b0c
TooCoolForStool <dragonofdagon@gmail.com>
2016-09-22 07:19:53 -0400

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class Form1
+    Public WeaponFind As Integer
+    Private Sub Run_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RunAttempt.Click
+        Dim Run As Object
+        Run = RunAttempt
+        Randomize()
+        Dim RunChance As Integer = CInt(Int((100 * Rnd()) + 1))
+        If RunChance > 75 Then
+            MessageBox.Show("You escaped...")
+            Close()
+        Else
+            MessageBox.Show("You failed to escape!")
+        End If
+        Me.EnemyControl_Click(Me.EnemyControl, EventArgs.Empty)
+        RunAttempt.Visible = False
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        EnemyHealth.Value = 100
+        PlayerHealth.Value = 10
+        MessageBox.Show("An Orc challenges you to a fight!")
+    End Sub
+    Public Sub Swing_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Swing.Click
+        Randomize()
+        Dim HitChance As Integer = CInt(Int((100 * Rnd()) + 1))
+        Randomize()
+
+        If HitChance > 45 Then
+
+            If Weaponfind = 5 Then
+                MessageBox.Show("You shoot it!")
+                Randomize()
+                Dim GunHitValue As Integer = CInt(Int((80 * Rnd()) + 40))
+                EnemyHealth.Increment(-GunHitValue)
+            ElseIf Weaponfind = 4 Then
+                MessageBox.Show("You swing for the fences!")
+                Randomize()
+                Dim StickHitValue As Integer = CInt(Int((50 * Rnd()) + 25))
+                EnemyHealth.Increment(-StickHitValue)
+            ElseIf Weaponfind = 3 Then
+                MessageBox.Show("You stab into the foe!")
+                Randomize()
+                Dim ShankHitValue As Integer = CInt(Int((35 * Rnd()) + 10))
+                EnemyHealth.Increment(-ShankHitValue)
+            Else
+                MessageBox.Show("You strike the enemy!")
+                Randomize()
+                Dim FistHitValue As Integer = CInt(Int((20 * Rnd()) + 5))
+                EnemyHealth.Increment(-FistHitValue)
+            End If
+        Else
+            MessageBox.Show("You missed...")
+        End If
+        If EnemyHealth.Value = 0 Then
+            MessageBox.Show("You win!")
+            Close()
+        End If
+        Me.EnemyControl_Click(Me.EnemyControl, EventArgs.Empty)
+    End Sub
+    Private Sub EnemyHealth_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyHealth.Click
+        Dim EnemyHealth As ProgressBar
+        EnemyHealth = New ProgressBar()
+        EnemyHealth.Minimum = 0
+        EnemyHealth.Maximum = 100
+        EnemyHealth.Value = 100
+    End Sub
+
+    Private Sub PlayerHealth_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PlayerHealth.Click
+        Dim PlayerHealth As ProgressBar
+        PlayerHealth = New ProgressBar()
+        PlayerHealth.Minimum = 0
+        PlayerHealth.Maximum = 10
+        PlayerHealth.Value = 10
+
+    End Sub
+    Private Sub EnemyControl_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyControl.Click
+        Dim EnemyHitChance As Integer = CInt(Int((100 * Rnd()) + 1))
+        MessageBox.Show("The Orc attacks with his mace..!")
+        If EnemyHitChance > 60 Then
+            MessageBox.Show("The Orc strikes you!")
+            PlayerHealth.Increment(-1)
+        Else
+            MessageBox.Show("The Enemy missed...")
+        End If
+        If PlayerHealth.Value = 0 Then
+            MessageBox.Show("You died...")
+        End If
+    End Sub
+
+    Public Sub Weapon_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Weapon.Click
+        MessageBox.Show("You look for a weapon...")
+        Randomize()
+        Dim WeaponFind2 As Integer = CInt(Int((5 * Rnd()) + 1))
+        WeaponFind = WeaponFind2
+        If WeaponFind < 3 Then
+            MessageBox.Show("You failed to find a weapon...")
+        End If
+        If WeaponFind = 3 Then
+            MessageBox.Show("You found a shank!")
+        ElseIf WeaponFind = 4 Then
+            MessageBox.Show("You found a 2x4!")
+        ElseIf WeaponFind = 5 Then
+            MessageBox.Show("You find a gun!")
+        End If
+        If WeaponFind > 2 Then
+            Weapon.Visible = False
+        End If
+        Me.EnemyControl_Click(Me.EnemyControl, EventArgs.Empty)
+    End Sub
+End Class
---
+++ MiniGame- Medium.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RunAttempt = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Swing = New System.Windows.Forms.Button()
+        Me.EnemyHealth = New System.Windows.Forms.ProgressBar()
+        Me.PlayerHealth = New System.Windows.Forms.ProgressBar()
+        Me.EnemyControl = New System.Windows.Forms.Button()
+        Me.Weapon = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RunAttempt
+        '
+        Me.RunAttempt.Location = New System.Drawing.Point(259, 254)
+        Me.RunAttempt.Name = "RunAttempt"
+        Me.RunAttempt.Size = New System.Drawing.Size(75, 23)
+        Me.RunAttempt.TabIndex = 0
+        Me.RunAttempt.Text = "Run"
+        Me.RunAttempt.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(135, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Umber Hulk"
+        '
+        'Swing
+        '
+        Me.Swing.Location = New System.Drawing.Point(12, 175)
+        Me.Swing.Name = "Swing"
+        Me.Swing.Size = New System.Drawing.Size(75, 23)
+        Me.Swing.TabIndex = 2
+        Me.Swing.Text = "Swing"
+        Me.Swing.UseVisualStyleBackColor = True
+        '
+        'EnemyHealth
+        '
+        Me.EnemyHealth.Location = New System.Drawing.Point(52, 91)
+        Me.EnemyHealth.Name = "EnemyHealth"
+        Me.EnemyHealth.Size = New System.Drawing.Size(221, 23)
+        Me.EnemyHealth.TabIndex = 3
+        '
+        'PlayerHealth
+        '
+        Me.PlayerHealth.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PlayerHealth.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.PlayerHealth.Location = New System.Drawing.Point(12, 262)
+        Me.PlayerHealth.MarqueeAnimationSpeed = 10
+        Me.PlayerHealth.Maximum = 10
+        Me.PlayerHealth.Name = "PlayerHealth"
+        Me.PlayerHealth.Size = New System.Drawing.Size(110, 15)
+        Me.PlayerHealth.TabIndex = 4
+        '
+        'EnemyControl
+        '
+        Me.EnemyControl.Location = New System.Drawing.Point(285, 12)
+        Me.EnemyControl.Name = "EnemyControl"
+        Me.EnemyControl.Size = New System.Drawing.Size(49, 42)
+        Me.EnemyControl.TabIndex = 5
+        Me.EnemyControl.Text = "Enemy Control"
+        Me.EnemyControl.UseVisualStyleBackColor = True
+        Me.EnemyControl.Visible = False
+        '
+        'Weapon
+        '
+        Me.Weapon.Location = New System.Drawing.Point(255, 175)
+        Me.Weapon.Name = "Weapon"
+        Me.Weapon.Size = New System.Drawing.Size(79, 23)
+        Me.Weapon.TabIndex = 6
+        Me.Weapon.Text = "Find Weapon"
+        Me.Weapon.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 246)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(26, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "You"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(346, 311)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Weapon)
+        Me.Controls.Add(Me.EnemyControl)
+        Me.Controls.Add(Me.PlayerHealth)
+        Me.Controls.Add(Me.EnemyHealth)
+        Me.Controls.Add(Me.Swing)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RunAttempt)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RunAttempt As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Swing As System.Windows.Forms.Button
+    Friend WithEvents EnemyHealth As System.Windows.Forms.ProgressBar
+    Friend WithEvents PlayerHealth As System.Windows.Forms.ProgressBar
+    Friend WithEvents EnemyControl As System.Windows.Forms.Button
+    Friend WithEvents Weapon As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
